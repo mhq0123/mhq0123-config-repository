1a1caff7dc105551870f7a6f14cdda8fbaaff498
mhq0123 <532386274@qq.com>
2016-12-25 20:25:42 +0800

    添加mysql配置

---
+++ mysql/normal.sql
@@ -0,0 +1,6 @@
+-- 创建用户,名字为userone,密码为123456
+　　create user userone identified by '123456';
+-- 创建名字为dbone的数据库
+　　create database dbone;
+-- 授予dbone数据库的所有权限给userone用户：
+　　grant all on dbone.* to userone;
